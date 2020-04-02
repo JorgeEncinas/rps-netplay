a0b70e868e4fc0df0ae5983c63427dc4a52504ae
AsombroSoft <50152158+AsombroSoft@users.noreply.github.com>
2020-04-01 18:44:05 -0700

    Add files via upload
    Piedra Papel o Tijeras entre 2 jugadores solamente.

--- client.py
+++ client.py
@@ -6,11 +6,45 @@ def despliega_menu():
     print("MENU")
     print("1.Iniciar jugada")
     print("2.Preguntar si hay más jugadores")
-    print("3.Mostrar partida")
+    print("3.Crear partida") #Cambie mostrar partida por Crear partida, no se puede jugar si no se presiona 3 antes.
+    print("4.jugar")
     print("0.Salir")
     o = input("Opcion:>")
     return int(o)
 
+def RPS(manita, manota):
+    ganador=""
+    if (manita[1]==manota[1]): ganador="EMPATE"
+
+    if (manita[1] == 'Piedra' and manota[1] == 'Papel'): ganador = manota[0]
+    if (manita[1] == 'Piedra' and manota[1] == 'Tijera'): ganador = manita[0]
+
+    if (manita[1] == 'Papel' and manota[1] == 'Piedra'): ganador = manita[0]
+    if (manita[1] == 'Papel' and manota[1] == 'Tijera'): ganador = manota[0]
+
+    if (manita[1] == 'Tijera' and manota[1] == 'Piedra'): ganador = manota[0]
+    if (manita[1] == 'Tijera' and manota[1] == 'Papel'): ganador = manita[0]
+
+    return ganador
+
+
+def juez(jugadores, proxy):
+    #en el diccionario el nombre del jugador se representa como j y su jugada es jugadores[j])
+    tuplas=jugadores.items()
+    participes=[]
+    for j in tuplas:
+        participes.append(j)
+    winner=RPS(participes[0], participes[1])
+    #Para juegos de muchos jugadores  se podria llamar RPS dentro de un loop????
+    print("EL GANADOR ES... ", winner)
+
+    if (winner=="EMPATE"):
+    #llama a jugada para que le de una nueva mano al jugador
+        jugada(proxy)
+
+def jugada(proxy):
+    j=proxy.agrega_jugador(jugador)
+    print(j)
 
 def main(jugador):
     print("Iniciamos!")
@@ -22,14 +56,19 @@ def main(jugador):
             if opcion == 0:
                 break
             if opcion == 1:
-                j = proxy.agrega_jugador(jugador)
-                print(j)
+                jugada(proxy)
+
             if opcion == 2:
                 n = proxy.numero_jugadores()
                 print("Jugadores:",n)
             if opcion == 3:
                 d = proxy.deck()
                 print(d)
+            if opcion == 4:
+                try:
+                    juez(d,proxy)
+                except:
+                    print("NO EXISTE PARTIDA")
         print("Saliendo")
 
     except ConnectionError:
